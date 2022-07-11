1de09041cdfd1c876f02d5d6197965699d8c994a
MathyasYazew <88359082+MathyasYazew@users.noreply.github.com>
2022-07-11 17:10:29 +0300

    webpack_config

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,4 +1,14 @@
+const path = require('path');
 
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+    publicPath: '/public'
+  },
+  mode: 'production',
+  module: {
 	rules: [
 		{
 			test: /\.css$/i,
